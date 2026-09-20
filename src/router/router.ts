afd4a1a332130f24743c042c270c2c29be3e4c07
Zhenni <zhenya.harutyunyan99@gmail.com>
2026-09-21 03:17:38 +0400

    fix: support GitHub Pages base path

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,11 +1,10 @@
 import { createHomePage } from '../pages/home/home';
 
 export const router = (): void => {
-  const path = window.location.pathname;
-  const app = document.querySelector<HTMLDivElement>('#app');
+    const app = document.querySelector<HTMLDivElement>('#app');
 
-  if (path === '/' && app) {
-    app.innerHTML = '';
-    app.append(createHomePage());
-  }
+    if (app) {
+        app.innerHTML = '';
+        app.append(createHomePage());
+    }
 };
